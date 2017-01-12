595800af7e3b53790d8e70f484829d2b4bf80109
Vinícius Figueiredo <viniciusfs@gmail.com>
2017-01-12 15:32:26 -0200

    Removed VMware text from messages

--- centos-7/scripts/vmware.sh
+++ centos-7/scripts/vmware.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 if [ $PACKER_BUILDER_TYPE = "vmware-iso" ]; then
-  /bin/echo "Installing VMware tools."
+  /bin/echo "Installing OpenVM tools."
 
   /bin/yum install -y open-vm-tools
 
   if [[ $? -eq 0 ]]; then
-    /bin/echo "VMware tools successfully installed."
+    /bin/echo "OpenVM tools successfully installed."
   else
-    /bin/echo "Errors were found during VMware tools installation."
+    /bin/echo "Errors were found during OpenVM tools installation."
   fi
 else
-  /bin/echo "This is not a VMware machine, not installing VMware Tools."
+  /bin/echo "This is not a VMX machine, not installing OpenVM tools."
 fi
